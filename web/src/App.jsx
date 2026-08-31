a06d598c64d58f8ce55aa39d46dcd8d9aee53477
dhanapalsgithub <dhanapaul2020@gmail.com>
2026-08-31 21:59:21 +0530

    updatee

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import HomePage from './pages/HomePage';
 
 function App() {
+    useEffect(() => {
+        // Microsoft Clarity Tracking Script
+        (function(c, l, a, r, i, t, y){
+            c[a] = c[a] || function(){(c[a].q = c[a].q ||[]).push(arguments)};
+            t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+            y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+        })(window, document, "clarity", "script", "yb2hm66o1z");
+    }, []);
+
     return (
         <Router>
             <ScrollToTop />
